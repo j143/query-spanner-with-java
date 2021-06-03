b4f160da120a80db5ef10064d2daffbd85e316ef
j143 <j143@protonmail.com>
2021-06-03 03:50:00 +0000

    create container for Song class

--- spanner-with-java/src/main/java/page/janardhan/labs/App.java
+++ spanner-with-java/src/main/java/page/janardhan/labs/App.java
@@ -6,6 +6,25 @@ package page.janardhan.labs;
  */
 public class App 
 {
+    
+    static class Song {
+        final int songId;
+        final String title;
+        final int year;
+        final int peak;
+
+        Song(int songId, String title, int year, int peak) {
+            this.songId = songId;
+            this.title = title;
+            this.year = year;
+            this.peak = peak;
+        }
+    }
+
+    static class Singer {
+        
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
